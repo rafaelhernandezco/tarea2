ab47388a624d8a527f4c1d07e553b1b04d9151f9
rafaelhernandezco <rafael.hcarrasco30@gmail.com>
2015-09-02 12:55:30 -0500

    cambio y fuera

--- sherlock.js
+++ sherlock.js
@@ -22,3 +22,46 @@ _INPUT = "2\n3 9\n17 24";
 */
 
 //----- Start your code here -------
+
+console.log(""); // imprime
+console.log("Rangos:"); //impre el nombre de los rangos
+console.log(_INPUT); //imprime el numero de rangos y los rangos anteriormente establecidos
+_INPUT = _INPUT.split("\n"); //divide el arreglo y excluye las \n
+T = _INPUT.length - 1; //le resta 1 a la longitud del input
+console.log(""); //imprime
+console.log("Cuadrados perfectos por rango:"); //imprime el label de los cuadrados que hay por caso
+
+for (contador = 0; contador < T; contador++) //ciclo del contador T
+{
+
+	rango = _INPUT[contador+1];
+
+	rango2 = rango.split(" ");
+
+	contador2=0;
+	
+	if(1 <= rango2[0] && rango2[0] <= rango2[1] && rango2[1] <= 1000000000) //condiciones de rango
+	{
+
+		for(contador3 = rango2[0]; contador3 <= rango2[1]; contador3++) //ciclo de los rangos
+		{
+
+			cuadrado = Math.sqrt(contador3); //sacar el cuadrado
+
+			cuadrado = cuadrado%1;
+			
+			if(cuadrado==0) //condicion del cuadrado
+			{
+
+				contador2 = contador2+1;
+
+			}
+
+		} 
+
+	console.log(contador2);
+
+	}
+
+}
+console.log("");
